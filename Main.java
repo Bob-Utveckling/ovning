62ff5ad985382cc0644f5d863dcad24c49be5cbb
Sebastian Aronsson <sebastian.aronsson@iths.se>
2018-11-14 08:55:07 +0000

    adding add method

--- Main.java
+++ Main.java
@@ -2,6 +2,11 @@ Class Main {
 
   public static void main (String[] args){
   System.out.println("Test");
+  System.out.println(add(10,20));
+  }
+
+  public static int add(int a, int b){
+    return a + b;
   }
 
 }
